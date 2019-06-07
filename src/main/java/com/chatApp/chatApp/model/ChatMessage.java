4ec814a0fc9e8744046032cf0457c52513dd6dee
ekta1999 <ektamishra1999@gmail.com>
2019-06-07 12:45:48 +0530

    df

---
+++ src/main/java/com/chatApp/chatApp/model/ChatMessage.java
@@ -0,0 +1,37 @@
+package com.chatApp.chatApp.model;
+
+public class ChatMessage {
+    private MessageType type;
+    private String content;
+    private String sender;
+
+    public enum MessageType {
+        CHAT,
+        JOIN,
+        LEAVE
+    }
+
+    public MessageType getType() {
+        return type;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setType(MessageType type) {
+        this.type = type;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+}
